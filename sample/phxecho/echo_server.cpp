34648d48c65e1536b350b52c0c2b850b70451415
wangwei90 <wangwei90@baidu.com>
2018-01-30 11:46:03 +0800

    feat(phxecho): 增加演示不带 SMCtx 参数 propose() 的用法

--- sample/phxecho/echo_server.cpp
+++ sample/phxecho/echo_server.cpp
@@ -1,22 +1,22 @@
 /*
-Tencent is pleased to support the open source community by making 
+Tencent is pleased to support the open source community by making
 PhxPaxos available.
-Copyright (C) 2016 THL A29 Limited, a Tencent company. 
+Copyright (C) 2016 THL A29 Limited, a Tencent company.
 All rights reserved.
 
-Licensed under the BSD 3-Clause License (the "License"); you may 
-not use this file except in compliance with the License. You may 
+Licensed under the BSD 3-Clause License (the "License"); you may
+not use this file except in compliance with the License. You may
 obtain a copy of the License at
 
 https://opensource.org/licenses/BSD-3-Clause
 
-Unless required by applicable law or agreed to in writing, software 
-distributed under the License is distributed on an "AS IS" basis, 
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
-implied. See the License for the specific language governing 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing
 permissions and limitations under the License.
 
-See the AUTHORS file for names of contributors. 
+See the AUTHORS file for names of contributors.
 */
 
 #include "echo_server.h"
@@ -34,7 +34,8 @@ namespace phxecho
 {
 
 PhxEchoServer :: PhxEchoServer(const phxpaxos::NodeInfo & oMyNode, const phxpaxos::NodeInfoList & vecNodeList)
-    : m_oMyNode(oMyNode), m_vecNodeList(vecNodeList), m_poPaxosNode(nullptr)
+    : m_oMyNode(oMyNode), m_vecNodeList(vecNodeList), m_poPaxosNode(nullptr),
+      sm1_("SM1", 1), sm2_("SM2", 2)
 {
 }
 
@@ -53,10 +54,10 @@ int PhxEchoServer :: MakeLogStoragePath(std::string & sLogStoragePath)
     if (access(sLogStoragePath.c_str(), F_OK) == -1)
     {
         if (mkdir(sLogStoragePath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1)
-        {       
+        {
             printf("Create dir fail, path %s\n", sLogStoragePath.c_str());
             return -1;
-        }       
+        }
     }
 
     return 0;
@@ -82,7 +83,8 @@ int PhxEchoServer :: RunPaxos()
     GroupSMInfo oSMInfo;
     oSMInfo.iGroupIdx = 0;
     //one paxos group can have multi state machine.
-    oSMInfo.vecSMList.push_back(&m_oEchoSM);
+    oSMInfo.vecSMList.push_back(&sm1_);
+    oSMInfo.vecSMList.push_back(&sm2_);
     oOptions.vecGroupSMInfoList.push_back(oSMInfo);
 
     //use logger_google to print log
@@ -117,23 +119,33 @@ int PhxEchoServer :: Echo(const std::string & sEchoReqValue, std::string & sEcho
     oCtx.m_pCtx = (void *)&oEchoSMCtx;
 
     uint64_t llInstanceID = 0;
-    int ret = m_poPaxosNode->Propose(0, sEchoReqValue, llInstanceID, &oCtx);
-    if (ret != 0)
-    {
-        printf("paxos propose fail, ret %d\n", ret);
-        return ret;
+    if (sEchoReqValue.find("NOSMCTX") != std::string::npos) {
+        int ret = m_poPaxosNode->Propose(0, sEchoReqValue, llInstanceID);
+        if (ret != 0)
+        {
+            printf("paxos propose fail, ret %d\n", ret);
+            return ret;
+        }
+
+        sEchoRespValue = "[NOSMCTX]" + sEchoReqValue;
+    } else {
+        int ret = m_poPaxosNode->Propose(0, sEchoReqValue, llInstanceID, &oCtx);
+        if (ret != 0)
+        {
+            printf("paxos propose fail, ret %d\n", ret);
+            return ret;
+        }
+
+        if (oEchoSMCtx.iExecuteRet != 0)
+        {
+            printf("echo sm excute fail, excuteret %d\n", oEchoSMCtx.iExecuteRet);
+            return oEchoSMCtx.iExecuteRet;
+        }
+
+        sEchoRespValue = oEchoSMCtx.sEchoRespValue.c_str();
     }
-
-    if (oEchoSMCtx.iExecuteRet != 0)
-    {
-        printf("echo sm excute fail, excuteret %d\n", oEchoSMCtx.iExecuteRet);
-        return oEchoSMCtx.iExecuteRet;
-    }
-
-    sEchoRespValue = oEchoSMCtx.sEchoRespValue.c_str();
-
     return 0;
 }
-    
+
 }
 
